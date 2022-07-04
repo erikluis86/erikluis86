4a0833778d4c8d30847876c1ffda55abbc735c41
Your Nameerik <erikedition@yahoo.com>
2022-07-04 14:08:50 -0400

    video 4 terminado

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import * as fromComponents from './components'
+
+@NgModule({
+  declarations: [...fromComponents.components],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    SharedRoutingModule
+  ],
+  exports : [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ...fromComponents.components
+  ]
+})
+export class SharedModule { }
