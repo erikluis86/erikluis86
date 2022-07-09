83d1f26a1e1192a525ef50df580ff1e6d77db045
Your Nameerik <erikedition@yahoo.com>
2022-07-09 09:20:42 -0400

    terminamos video 10

--- src/app/modules/user/user-detail/user-detail.component.ts
+++ src/app/modules/user/user-detail/user-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,  } from '@angular/router';
+import USERS_DATA from '@data/constants/users.const';
 import { ITitleh1 } from '@data/interfaces/title.metadata';
+import { UserInterfaz } from '@data/interfaces/user.metadata';
 
 
 
@@ -14,14 +16,19 @@ export class UserDetailComponent implements OnInit {
   public title : ITitleh1 = {
     text:"Detalle de Usuario",
     type:'secondary'
-  } ;
+  };
 
   public id : number = 0;
-   public description:string = 'Cada trabajador es fundamental';
+  public description:string = 'Donde cada trabajador es fundamental';
+  public users:UserInterfaz[] = USERS_DATA;
+  public currentUser : any ;
 
-   
+   // route nos sirve para atrapar el id del usuario
    constructor(private route: ActivatedRoute) {
-      this.id = this.route.snapshot.params['id']
+      this.id = +this.route.snapshot.params['id']
+      //aki obtenemos el usuario buscandolo en el arreglo
+      this.currentUser = this.users.find(user => user.id === this.id)
+      console.log(this.currentUser)
     }
 
   ngOnInit(): void {
